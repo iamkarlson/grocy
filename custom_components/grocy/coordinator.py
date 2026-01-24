bdbc157eab4173c5ac93120734c9c0a0eefd4b2f
ChrisLanks <ChrisLanks27@gmail.com>
2026-01-24 12:16:24 -0500

    Test (#4)
    * add calendar
    
    * add hard reload and translation
    
    * update
    
    * allow adding the apikey and port
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * ensure all day events show
    
    * update reconfigure
    
    * update reconfigure
    
    * update for local timezone
    
    * update for local timezone from utc
    
    * fix timedate config option
    
    * add option to fix grocy utc time
    
    * add debug logging
    
    * add better logs
    
    * rename config option
    
    ---------
    
    Co-authored-by: Chris Lanks <chris.lanks27@gmail.com>
    
    ---------
    
    Co-authored-by: Chris Lanks <chris.lanks27@gmail.com>

--- custom_components/grocy/coordinator.py
+++ custom_components/grocy/coordinator.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import logging
 from dataclasses import dataclass
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
@@ -57,6 +58,7 @@ class GrocyDataUpdateCoordinator(DataUpdateCoordinator[GrocyCoordinatorData]):
     def __init__(
         self,
         hass: HomeAssistant,
+        config_entry: ConfigEntry,
     ) -> None:
         """Initialize Grocy data update coordinator."""
         super().__init__(
@@ -65,6 +67,8 @@ class GrocyDataUpdateCoordinator(DataUpdateCoordinator[GrocyCoordinatorData]):
             name=DOMAIN,
             update_interval=SCAN_INTERVAL,
         )
+
+        self.config_entry = config_entry
 
         url = self.config_entry.data[CONF_URL]
         api_key = self.config_entry.data[CONF_API_KEY]
@@ -87,6 +91,10 @@ class GrocyDataUpdateCoordinator(DataUpdateCoordinator[GrocyCoordinatorData]):
         for entity in self.entities:
             if not entity.enabled:
                 _LOGGER.debug("Entity %s is disabled", entity.entity_id)
+                continue
+
+            # Skip calendar entity - it doesn't use coordinator data
+            if not hasattr(entity, "entity_description") or entity.entity_description.key == "calendar":
                 continue
 
             try:
